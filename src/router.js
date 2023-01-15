85b058674e20b2e52236a85401fe1f323e889448
Genet2327 <genetaddisalem@gmail.com>
2023-01-15 16:27:01 -0600

    First Commit

---
+++ src/router.js
@@ -0,0 +1,79 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+import Login from "./views/Login.vue";
+
+import TutorialsList from "./views/TutorialsList.vue";
+import EditTutorial from "./views/EditTutorial.vue";
+import AddTutorial from "./views/AddTutorial.vue";
+import ViewTutorial from "./views/ViewTutorial.vue";
+import AddLesson from "./views/AddLesson.vue";
+import EditLesson from "./views/EditLesson.vue";
+
+import RoleList from "./views/Role/RoleList.vue";
+import EditRole from "./views/Role/EditRole.vue";
+
+Vue.use(Router);
+
+const router = new Router({
+  mode: "hash",
+  linkExactActiveClass: "active",
+  base:
+    //    process.env.NODE_ENV === 'development'? "/" : "/tutorScheduling/", - for AWS
+    process.env.NODE_ENV === "development" ? "/" : "/",
+  routes: [
+    {
+      path: "/",
+      name: "login",
+      component: Login,
+    },
+    {
+      path: "/tutorials",
+      name: "tutorials",
+      component: TutorialsList,
+    },
+    {
+      path: "/edit/:id",
+      name: "edit",
+      component: EditTutorial,
+      props: true,
+    },
+    {
+      path: "/add",
+      name: "add",
+      component: AddTutorial,
+    },
+    {
+      path: "/view/:id",
+      name: "view",
+      component: ViewTutorial,
+      props: true,
+    },
+    {
+      path: "/addLesson/:tutorialId",
+      name: "addLesson",
+      component: AddLesson,
+      props: true,
+    },
+    {
+      path: "/editLesson/:tutorialId/:lessonId",
+      name: "editLesson",
+      component: EditLesson,
+      props: true,
+    },
+    {
+      path: "/RoleList",
+      name: "RoleList",
+      component: RoleList,
+    },
+   
+    {
+      path: "/edit-role/:id",
+      name: "EditRole",
+      component: EditRole,
+      props: true,
+    },
+  ],
+});
+
+export default router;
