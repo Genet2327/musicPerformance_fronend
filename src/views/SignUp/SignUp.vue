ecbfa5e38e8c2a257d80b813c822f895a1fad31b
Shawn Turner <shawn.m.turner@eagles.oc.edu>
2023-03-29 12:48:22 -0500

    formatting improvements

--- src/views/SignUp/SignUp.vue
+++ src/views/SignUp/SignUp.vue
@@ -9,140 +9,137 @@
       <br /><br />
       
       <form>
-    <v-text-field
-      v-model="name"
-      :error-messages="nameErrors"
-      :counter="10"
-      label="Name"
-      required
-      @input="$v.name.$touch()"
-      @blur="$v.name.$touch()"
-    ></v-text-field>
-
-<!-- Select Hearing Date -->
-
-    <v-menu
-    v-model="menu"
-    :close-on-content-click="false"
-    :nudge-right="40"
-    transition="scale-transition"
-    offset-y
-    min-width="290px"
-  >
-    <template v-slot:activator="{ on }">
-      <v-text-field
-        v-model="date"
-        label="Select a hearing date"
-        prepend-icon="mdi-calendar"
-        readonly
-        v-on="on"
-      ></v-text-field>
-    </template>
-    <v-date-picker v-model="date" no-title scrollable>
-      <v-spacer></v-spacer>
-      <v-btn text color="primary" @click="menu = false">Cancel</v-btn>
-      <v-btn text color="primary" @click="menu = false">OK</v-btn>
-    </v-date-picker>
-  </v-menu>
-  
-<!-- Select Hearing Time -->
-
-  <v-menu
-    v-model="menu"
-    :close-on-content-click="false"
-    :nudge-right="40"
-    transition="scale-transition"
-    offset-y
-    min-width="290px"
-  >
-    <template v-slot:activator="{ on }">
-      <v-text-field
-        v-model="date"
-        label="Select a hearing time"
-        prepend-icon="mdi-calendar"
-        readonly
-        v-on="on"
-      ></v-text-field>
-    </template>
-    <v-date-picker v-model="date" no-title scrollable>
-      <v-spacer></v-spacer>
-      <v-btn text color="primary" @click="menu = false">Cancel</v-btn>
-      <v-btn text color="primary" @click="menu = false">OK</v-btn>
-    </v-date-picker>
-  </v-menu>
-  
-
-                            <!-- Add an accompanist to the performance -->
-                            <v-row>
-                                <v-col>
-                                    <v-select
-                                        :disabled="noAccompanist"
-                                        v-model="selectedAccompanist"
-                                        :items="accompanists"
-                                        item-text="fullName"
-                                        item-value="id"
-                                        label="Accompanist"
-                                    ></v-select>
-                                </v-col>
-                                <v-col cols="1">
-                                    <v-checkbox
-                                        v-model="noAccompanist"
-                                        label="None"
-                                        hide-details
-                                    ></v-checkbox>
-                                </v-col>
-                            </v-row>
-
-                            <!-- Add an Private Instructor to the performance -->
-                            <v-row>
-                                <v-col>
-                                    <v-select
-                                        :disabled="noPrivateInstructor"
-                                        v-model="selectedPrivateInstructor"
-                                        :items="PrivateInstructor"
-                                        item-text="fullName"
-                                        item-value="id"
-                                        label="Private Instructor"
-                                    ></v-select>
-                                </v-col>
-                                <v-col cols="1">
-                                    <v-checkbox
-                                        v-model="noAccompanist"
-                                        label="None"
-                                        hide-details
-                                    ></v-checkbox>
-                                </v-col>
-                            </v-row>
-
-                            <!-- Select the instrument -->
-                            <v-row>
-                                <v-select
-                                    label="Select Voice or Instrument"
-                                    v-model="selectedInstrumentId"
-                                    :items="instruments"
-                                    item-text="instrument"
-                                    item-value="id"
-                                ></v-select>
-                            </v-row>
-                            <div>
-                              <label>
-                                <input type="checkbox" v-model="isChecked">
-                                    If the text is in a Foreign Language Check
-                              </label>
-                            </div>
-                          
-    <div>
-    <button @click="saveData">Submit</button>
-  </div>
-
-          
-
     
-  </form>
+        <!-- Select Hearing Date -->
     
+        <v-menu
+          v-model="menu"
+          :close-on-content-click="false"
+          :nudge-right="40"
+          transition="scale-transition"
+          offset-y
+          min-width="290px"
+        >
+          <template v-slot:activator="{ on }">
+            <v-text-field
+              v-model="date"
+              label="Select a hearing date"
+              prepend-icon="mdi-calendar"
+              readonly
+              v-on="on"
+            ></v-text-field>
+          </template>
+          <v-date-picker v-model="date" no-title scrollable>
+            <v-spacer></v-spacer>
+            <v-btn text color="primary" @click="menu = false">Cancel</v-btn>
+            <v-btn text color="primary" @click="menu = false">OK</v-btn>
+          </v-date-picker>
+        </v-menu>
+  
+        <!-- Select Hearing Time -->
+  
+        <v-menu
+          v-model="menu"
+          :close-on-content-click="false"
+          :nudge-right="40"
+          transition="scale-transition"
+          offset-y
+          min-width="290px"
+        >
+          <template v-slot:activator="{ on }">
+            <v-text-field
+              v-model="date"
+              label="Select a hearing time"
+              prepend-icon="mdi-calendar"
+              readonly
+              v-on="on"
+            ></v-text-field>
+          </template>
+          <v-date-picker v-model="date" no-title scrollable>
+            <v-spacer></v-spacer>
+            <v-btn text color="primary" @click="menu = false">Cancel</v-btn>
+            <v-btn text color="primary" @click="menu = false">OK</v-btn>
+          </v-date-picker>
+        </v-menu>
+  
+        <!-- Add an accompanist to the performance -->
+        <v-row>
+          <v-col>
+            <v-select
+              :disabled="noAccompanist"
+              v-model="selectedAccompanist"
+              :items="accompanists"
+              item-text="fullName"
+              item-value="id"
+              label="Accompanist"
+            ></v-select>
+          </v-col>
+        </v-row>
+  
+        <!-- Add an Private Instructor to the performance -->
+        <v-row>
+          <v-col>
+            <v-select
+              :disabled="noPrivateInstructor"
+              v-model="selectedPrivateInstructor"
+              :items="PrivateInstructor"
+              item-text="fullName"
+              item-value="id"
+              label="Private Instructor"
+            ></v-select>
+          </v-col>
+        </v-row>
+  
+        <!-- Select the instrument -->
+        <v-row>
+          <v-select
+            label="Select Voice or Instrument"
+            v-model="selectedInstrumentId"
+            :items="instruments"
+            item-text="instrument"
+            item-value="id"
+          ></v-select>
+        </v-row>
+  
+        <v-text-field
+          v-model="SelectionTitle"
+          :error-messages="SelectionTitle"
+          :counter="50"
+          label="Selection Title"
+          required
+          @input="$v.SelectionTitle.$touch()"
+          @blur="$v.SelectionTitle.$touch()"
+        ></v-text-field>
+
+
+        <v-row>
+          <v-col>
+            <v-select
+              :disabled="noPrivateInstructor"
+              v-model="selectedPrivateInstructor"
+              :items="PrivateInstructor"
+              item-text="fullName"
+              item-value="id"
+              label="Private Instructor"
+            ></v-select>
+          </v-col>
+        </v-row>
+
+        <div>
+          <label>
+            <input type="checkbox" v-model="isChecked">
+            If the text is in a Foreign Language Check
+          </label>
+        </div>
+
+        <div>
+          <button @click="saveData">Submit</button>
+        </div>
+      </form>
     </v-container>
   </div>
 </template>
+
 <script>
 export default {
   data() {
