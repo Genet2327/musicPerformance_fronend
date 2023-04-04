48bdb582a93ad278956a4a241766363707fa89cc
selamawit <selamawit.eshete@eagles.oc.edu>
2023-04-03 23:23:06 -0500

    fix-critique

--- src/views/critique/Add.vue
+++ src/views/critique/Add.vue
@@ -1,141 +1,131 @@
 <template>
-  <div>
-    <v-container>
-      <v-toolbar>
-        <v-toolbar-title>Add Critique</v-toolbar-title>
+    <div>
+        <v-container>
+            <v-toolbar>
+                <v-toolbar-title>Add Critique</v-toolbar-title>
 
-      </v-toolbar>
+            </v-toolbar>
 
-      <br />
-      <h4>{{ message }}</h4>
-      <br />
+            <br />
+            <h4>{{ message }}</h4>
+            <br />
+
+            <!-- <v-form ref="form" v-model="valid" lazy validation>
+                <v-text-field v-model="critique.deportment" id="name" :counter="50"
+                    label="Stage Deportment (Pois, entrance)" required>
+                </v-text-field>
+            </v-form> -->
+            <v-form ref="form" v-model="valid" lazy validation>
+                <v-text-field v-model="critique.stagedeportment" id="name" :counter="50" label=" Stage Deportment (Pois, entrance)" required>
+                </v-text-field>
+            </v-form>
+
+            <v-form ref="form" v-model="valid" lazy validation>
+                <v-text-field v-model="critique.tone" id="name" :counter="50" label=" Tone" required>
+                </v-text-field>
+            </v-form>
+
+            <v-form ref="form" v-model="valid" lazy validation>
+                <v-text-field v-model="critique.accuracy" id="name" :counter="50" label="Accuracy" required>
+                </v-text-field>
+            </v-form>
 
 
+            <v-form ref="form" v-model="valid" lazy validation>
+                <v-text-field v-model="critique.technique" id="name" :counter="50" label="Technique" required>
+                </v-text-field>
+            </v-form>
 
-      <v-form ref="form" v-model="valid" lazy validation>
-        <v-text-field v-model="critique.department" id="name" :counter="50" label="Department" required>
-        </v-text-field>
-      </v-form>
+            <v-form ref="form" v-model="valid" lazy validation>
+                <v-text-field v-model="critique.interpretation" id="name" :counter="50" label="Interpretation" required>
+                </v-text-field>
+
+            </v-form>
+
+            <v-form ref="form" v-model="valid" lazy validation>
+                <v-text-field v-model="critique.balanceblend" id="name" :counter="50" label="Balance Blend" required>
+                </v-text-field>
+
+            </v-form>
+
+            <v-form ref="form" v-model="valid" lazy validation>
+                <v-text-field v-model="critique.diction" id="name" :counter="50" label="Diction (vowels,consonants,tonge)"
+                    required>
+                </v-text-field>
+            </v-form>
 
 
+            <v-form ref="form" v-model="valid" lazy validation>
+                <textarea v-model="Performance" placeholder="Performance and Recommendations"
+                    style="width: 1200px; height: 200px;"></textarea>
 
-      <v-form ref="form" v-model="valid" lazy validation>
-        <v-text-field v-model="critique.tone" id="name" :counter="50" label=" Tone" required>
-        </v-text-field>
+                <v-btn :disabled="!valid" color="success" class="mr-4" @click="saveCritique()">
+                    Save
+                </v-btn>
 
-      </v-form>
-
-      <v-form ref="form" v-model="valid" lazy validation>
-        <v-text-field v-model="critique.acurracy" id="name" :counter="50" label="Accuracy" required>
-        </v-text-field>
-      </v-form>
-
-
-      <v-form ref="form" v-model="valid" lazy validation>
-        <v-text-field v-model="critique.technique" id="name" :counter="50" label="Technique" required>
-        </v-text-field>
-      </v-form>
-
-      <v-form ref="form" v-model="valid" lazy validation>
-        <v-text-field v-model="critique.interpretation" id="name" :counter="50" label="Interpretation" required>
-        </v-text-field>
-
-      </v-form>
-
-      <v-form ref="form" v-model="valid" lazy validation>
-        <v-text-field v-model="critique.balanceblend" id="name" :counter="50" label="Balance Blend" required>
-        </v-text-field>
-
-      </v-form>
-
-      <v-form ref="form" v-model="valid" lazy validation>
-        <v-text-field v-model="critique.diction" id="name" :counter="50" label="Diction (vocal)" required>
-        </v-text-field>
-      </v-form>
-
-      <v-form ref="form" v-model="valid" lazy validation>
-        <v-text-field v-model="critique.performanc" id="name" :counter="70" label="Performance/ Suggestions" required>
-        </v-text-field>
-
-        <v-btn :disabled="!valid" color="success" class="mr-4" @click="saveCritique()">
-          Save
-        </v-btn>
-
-        <v-btn color="error" class="mr-4" @click="cancel()"> Cancel </v-btn>
-      </v-form>
-    </v-container>
-  </div>
+                <v-btn color="error" class="mr-4" @click="cancel()"> Cancel </v-btn>
+            </v-form>
+        </v-container>
+    </div>
 </template>
-  
+    
 <script>
 import CritiqueServices from "../../services/Critique/services";
 import Utils from "@/config/utils.js";
-
 export default {
-  name: "add-critique",
-  data() {
-    return {
-      valid: false,
-      user: {},
-      critique: {
-        id: null,
-        department: "",
-        tone: "",
-        acurracy: "",
-        technique: "",
-        interpretation: "",
-        balanceblend: "",
-        diction: "",
-        performanc: "",
-
-      },
-      message: "Enter data and click save",
-    };
-
-  },
-
-
-  mounted() {
-    this.user = Utils.getStore("user");
-
-  },
-
-  methods: {
-
-    saveCritique() {
-      var data = {
-        department: this.critique.department,
-        tone: this.critique.tone,
-        acurracy: this.critique.acurracy,
-        technique: this.critique.technique,
-        interpretation: this.critique.interpretation,
-        balanceblend: this.critique.balanceblend,
-        diction: this.critique.diction,
-        performanc: this.critique.performanc,
-        userId: this.user.userId,
-      };
-
-      console.log(data);
-      CritiqueServices.create(data)
-        .then((response) => {
-          this.critique.id = response.data.id;
-          console.log("add " + response.data);
-          this.$router.push({ name: "CritiqueList" });
-        })
-        .catch((e) => {
-          console.log("add Eroro" + e.response.data.message);
-          this.message = e.response.data.message;
-        });
+    name: "add-critique",
+    data() {
+        return {
+            valid: false,
+            user: {},
+            critique: {
+                id: null,
+                stagedeportment: "",
+                tone: "",
+                accuracy: "",
+                technique: "",
+                interpretation: "",
+                balanceblend: "",
+                diction: "",
+                performance: "",
+            },
+            message: "Enter data and click save",
+        };
     },
-    cancel() {
-      console.log("add");
-
-      this.$router.push({ name: "CritiqueList" });
+    mounted() {
+        this.user = Utils.getStore("user");
     },
-
-
-  },
+    methods: {
+        saveCritique() {
+            var data = {
+                stagedeportment: this.critique.stagedeportment,
+                tone: this.critique.tone,
+                accuracy: this.critique.accuracy,
+                technique: this.critique.technique,
+                interpretation: this.critique.interpretation,
+                balanceblend: this.critique.balanceblend,
+                diction: this.critique.diction,
+                performance: this.critique.performance,
+                userId: this.user.userId,
+            };
+            console.log(data);
+            CritiqueServices.create(data)
+                .then((response) => {
+                    this.critique.id = response.data.id;
+                    console.log("add " + response.data);
+                    this.$router.push({ name: "CritiqueList" });
+                })
+                .catch((e) => {
+                    console.log("add Eroro" + e.response.data.message);
+                    this.message = e.response.data.message;
+                });
+        },
+        cancel() {
+            console.log("add");
+            this.$router.push({ name: "CritiqueList" });
+        },
+    },
 };
 </script>
 <style></style>
-  
+    
